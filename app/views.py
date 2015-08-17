2f6cdcaba771c0510bc7ab33d1cf229a7cdba6b3
Lidiya Murakhovs'ka <lidiya.murakhovskaya@gmail.com>
2015-08-17 13:39:50 +0300

    edit user profiles

--- app/views.py
+++ app/views.py
@@ -1,7 +1,9 @@
 from flask import render_template, flash, redirect, session, url_for, request, g
-from flask.ext.login import login_user, logout_user, current_user, login_required
+from flask.ext.login import login_user, logout_user, current_user, \
+    login_required
+from datetime import datetime
 from app import app, db, lm, oid
-from .forms import LoginForm
+from .forms import LoginForm, EditForm
 from .models import User
 
 
@@ -12,37 +14,32 @@ def load_user(id):
 
 @app.before_request
 def before_request():
-    g.user = current_user 	#current_user set by Flask-Login
+    g.user = current_user
+    if g.user.is_authenticated():
+        g.user.last_seen = datetime.utcnow()
+        db.session.add(g.user)
+        db.session.commit()
 
 
 @app.route('/')
 @app.route('/index')
 @login_required
 def index():
-	user = g.user
-	posts = [  # fake array of posts
-		{ 
-            'author': {'nickname': 'John'}, 
-            'body': 'Beautiful day in Portland!' 
+    user = g.user
+    posts = [
+        {
+            'author': {'nickname': 'John'},
+            'body': 'Beautiful day in Portland!'
         },
-        { 
-            'author': {'nickname': 'Susan'}, 
-            'body': 'The Avengers movie was so cool!' 
+        {
+            'author': {'nickname': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
         }
     ]
-	friends = [ #fake friends
-		{
-			'friend': {'nickname': 'Chris'},
-			'country': 'Malta',
-			'age': '18'
-		},
-		{
-			'friend': {'nickname': 'Naomi'},
-			'country': 'Spain',
-			'age': '17'
-		}
-	]
-	return render_template('index.html', title='Home', user=user, posts=posts, friends=friends)
+    return render_template('index.html',
+                           title='Home',
+                           user=user,
+                           posts=posts)
 
 
 @app.route('/login', methods=['GET', 'POST'])
@@ -54,19 +51,19 @@ def login():
     if form.validate_on_submit():
         session['remember_me'] = form.remember_me.data
         return oid.try_login(form.openid.data, ask_for=['nickname', 'email'])
-    return render_template('login.html', 
+    return render_template('login.html',
                            title='Sign In',
                            form=form,
                            providers=app.config['OPENID_PROVIDERS'])
 
 
 @oid.after_login
-def after_login(resp):  #resp contains info returned by the OpenID provider
-    if resp.email is None or resp.email == "":   #validation
+def after_login(resp):
+    if resp.email is None or resp.email == "":
         flash('Invalid login. Please try again.')
         return redirect(url_for('login'))
-    user = User.query.filter_by(email=resp.email).first() #search for email provided
-    if user is None:	 #consider this new user
+    user = User.query.filter_by(email=resp.email).first()
+    if user is None:
         nickname = resp.nickname
         if nickname is None or nickname == "":
             nickname = resp.email.split('@')[0]
@@ -77,8 +74,8 @@ def after_login(resp):  #resp contains info returned by the OpenID provider
     if 'remember_me' in session:
         remember_me = session['remember_me']
         session.pop('remember_me', None)
-    login_user(user, remember = remember_me)	#register this is a valid login
-    return redirect(request.args.get('next') or url_for('index'))	#redirect to the next/index page
+    login_user(user, remember=remember_me)
+    return redirect(request.args.get('next') or url_for('index'))
 
 
 @app.route('/logout')
@@ -91,7 +88,7 @@ def logout():
 @login_required
 def user(nickname):
     user = User.query.filter_by(nickname=nickname).first()
-    if user == None:
+    if user is None:
         flash('User %s not found.' % nickname)
         return redirect(url_for('index'))
     posts = [
@@ -101,3 +98,20 @@ def user(nickname):
     return render_template('user.html',
                            user=user,
                            posts=posts)
+
+
+@app.route('/edit', methods=['GET', 'POST'])
+@login_required
+def edit():
+    form = EditForm()
+    if form.validate_on_submit():
+        g.user.nickname = form.nickname.data
+        g.user.about_me = form.about_me.data
+        db.session.add(g.user)
+        db.session.commit()
+        flash('Your changes have been saved.')
+        return redirect(url_for('edit'))
+    else:
+        form.nickname.data = g.user.nickname
+        form.about_me.data = g.user.about_me
+    return render_template('edit.html', form=form)
